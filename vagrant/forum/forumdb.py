b899d7247cfc9b47fec7d18a21fac74e3ba79ee3
trashid <tariq@skyward.io>
2017-10-09 21:49:25 -0400

    Lesson 3.10 intro relational databases

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -1,15 +1,29 @@
 # "Database code" for the DB Forum.
 
 import datetime
+import psycopg2, bleach
 
-POSTS = [("This is the first post.", datetime.datetime.now())]
+#POSTS = [("This is the first post.", datetime.datetime.now())]
+#db = psycopg2.connect("dbname=forum")
+
 
 def get_posts():
-  """Return all posts from the 'database', most recent first."""
-  return reversed(POSTS)
+  db = psycopg2.connect("dbname=forum")
+  c = db.cursor()
+  query = "select content, time from posts order by time desc"
+  c.execute(query)
+  posts = c.fetchall()
+  db.close
+  return posts
 
 def add_post(content):
-  """Add a post to the 'database' with the current timestamp."""
-  POSTS.append((content, datetime.datetime.now()))
+  db = psycopg2.connect("dbname=forum")
+  c = db.cursor()
+  timestamp = datetime.datetime.now()
+  #query = "insert into posts values (content , timestamp) "
+  c.execute("insert into posts values (%s)" , (bleach.clean(content),))
+  db.commit()
+  db.close
+  
 
 
